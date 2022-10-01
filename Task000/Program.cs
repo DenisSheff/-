ff6251163ca90ecc0be32d1adc886bf35687e61e
Denis Shevchenko <denis_sheff@icloud.com>
2022-10-01 20:20:05 +0900

    git initialization

---
+++ Task000/Program.cs
@@ -0,0 +1,13 @@
+﻿/*
+Напишите программу, которая на вход
+принимает число и выдаёт его квадрат (число
+умноженное на само себя).
+Например:
+4 -> 16
+-3 -> 9
+-7 -> 49
+*/
+
+Console.Write("Enter your number: ");
+double number = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Square of {0} is {1}", number, Math.Pow(number, 2));
