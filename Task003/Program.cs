aa2f3c74ef8da6d8b78c192b502ef11b70ec432b
Denis Shevchenko <denis_sheff@icloud.com>
2022-10-02 20:43:42 +0900

    Task003 done

---
+++ Task003/Program.cs
@@ -0,0 +1,38 @@
+﻿/*
+Напишите программу, которая будет выдавать
+название дня недели по заданному номеру.
+3 -> Среда
+5 -> Пятница
+*/
+
+Console.Write("Enter weekday number: ");
+int WeekDayNumber = int.Parse(Console.ReadLine());
+
+if (WeekDayNumber == 1)
+{
+	Console.WriteLine("It is Monday!");
+}
+if (WeekDayNumber == 2)
+{
+	Console.WriteLine("It is Tuesday!");
+}
+if (WeekDayNumber == 3)
+{
+	Console.WriteLine("It is Wednesday!");
+}
+if (WeekDayNumber == 4)
+{
+	Console.WriteLine("It is Thursday!");
+}
+if (WeekDayNumber == 5)
+{
+	Console.WriteLine("It is Friyaaaaay!");
+}
+if (WeekDayNumber == 6)
+{
+	Console.WriteLine("It is Saturday!");
+}
+if (WeekDayNumber == 7)
+{
+	Console.WriteLine("It is Sunday!");
+}
