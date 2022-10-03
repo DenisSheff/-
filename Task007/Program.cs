5b4a6faae3d2bcd3bae8ca5c78c0de3134e8019f
Denis Shevchenko <denis_sheff@icloud.com>
2022-10-03 15:51:52 +0900

    Tasks 005 and 007  are done

---
+++ Task007/Program.cs
@@ -0,0 +1,12 @@
+﻿/*
+Напишите программу, которая принимает на вход
+трёхзначное число и на выходе показывает последнюю цифру
+этого числа.
+456 -> 6
+782 -> 2
+918 -> 8
+*/
+
+Console.Write("Enter your number: ");
+int number = int.Parse(Console.ReadLine());
+Console.WriteLine(number % 10);
