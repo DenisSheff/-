a5d415313247d42b5021029cb94d3b3da837b1a2
Denis Shevchenko <denis_sheff@icloud.com>
2022-10-03 23:04:18 +0900

    New folder

---
+++ home_task/Task004/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Clear();
+Console.Write("Enter any three digits: ");
+int firstNumber = int.Parse(Console.ReadLine());
+int secondNumber = int.Parse(Console.ReadLine());
+int thirdNumber = int.Parse(Console.ReadLine());
+
+int max = 0;
+
+if ((firstNumber > secondNumber) && (firstNumber > thirdNumber))
+{
+	max = firstNumber;
+}
+if ((secondNumber > firstNumber) && (secondNumber > thirdNumber))
+{
+	max = secondNumber;
+}
+if ((thirdNumber > firstNumber) && (thirdNumber > secondNumber))
+{
+	max = thirdNumber;
+}
+
+Console.WriteLine("Highest number is: " + max);
