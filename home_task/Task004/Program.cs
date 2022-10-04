b03aa11b3e14ea03258f432d4a805786f33d2dc7
Denis Shevchenko <denis_sheff@icloud.com>
2022-10-04 14:16:00 +0900

    Task 004 updated

--- home_task/Task004/Program.cs
+++ home_task/Task004/Program.cs
@@ -6,7 +6,9 @@ int thirdNumber = int.Parse(Console.ReadLine());
 
 int max = 0;
 
-if ((firstNumber > secondNumber) && (firstNumber > thirdNumber))
+/*
+ПЕРВОЕ РЕШЕНИЕ
+if ((firstNumber > secondNumber) && (firstNumber > secondNumber))
 {
 	max = firstNumber;
 }
@@ -17,6 +19,24 @@ if ((secondNumber > firstNumber) && (secondNumber > thirdNumber))
 if ((thirdNumber > firstNumber) && (thirdNumber > secondNumber))
 {
 	max = thirdNumber;
+}
+*/
+
+//ОПТИМИЗАЦИЯ РЕШЕНИЯ
+if (firstNumber > secondNumber)
+{
+	max = firstNumber;
+}
+else
+{
+	if (secondNumber > thirdNumber)
+	{
+		max = secondNumber;
+	}
+	else
+	{
+		max = thirdNumber;
+	}
 }
 
 Console.WriteLine("Highest number is: " + max);
