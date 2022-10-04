5e2c958b286f417b31ae9796e199eb81c2d36000
Denis Shevchenko <denis_sheff@icloud.com>
2022-10-04 14:11:33 +0900

    Task 002 updated

--- home_task/Task002/Program.cs
+++ home_task/Task002/Program.cs
@@ -3,13 +3,10 @@ Console.Write("Enter your first number: ");
 int firstNumber = int.Parse(Console.ReadLine());
 Console.Write("Enter your second number: ");
 int secondNumber = int.Parse(Console.ReadLine());
-int max;
 
-if (firstNumber > secondNumber)
-{
-	max = firstNumber;
-}
-else
+int max = firstNumber;
+
+if (firstNumber < secondNumber)
 {
 	max = secondNumber;
 }
