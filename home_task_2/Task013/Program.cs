4073eed0b17f796392dc2026470f6567177a48fb
Denis Shevchenko <denis_sheff@icloud.com>
2022-10-07 13:46:46 +0900

    Task 13 is done

---
+++ home_task_2/Task013/Program.cs
@@ -0,0 +1,25 @@
+﻿
+//Подскажите, как можно было уменьшить количество if'ов? Или направьте, в каком направлении смотреть. 
+
+Console.Clear();
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+if (number >= 100 && number <= 999)
+{
+	int thirdDigit = number % 10;
+	Console.WriteLine($"Third digit of a number {number} is {thirdDigit}");
+}
+else if (number < 100)
+{
+	Console.WriteLine($"Third digit of a number {number} is missing.");
+}
+else if (number < 10000)
+{
+	int thirdDigit = (number / 10) % 10;
+	Console.WriteLine($"Third digit of a number {number} is {thirdDigit}.");
+}
+else if (number < 100000)
+{
+	int thirdDigit = (number / 100) % 10;
+	Console.WriteLine($"Third digit of a number {number} is {thirdDigit}.");
+}
