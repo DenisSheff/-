0a0b5ac2d464992910ea601c1140e0f3344cd4fa
Denis Shevchenko <denis_sheff@icloud.com>
2022-10-11 15:41:44 +0900

    All data is added

---
+++ home_task_3/Task021/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+Console.Write("Enter number of X coordinate of the A line: ");
+double firstCoordinateXA = int.Parse(Console.ReadLine());
+Console.Write("Enter number of Y coordinate of the A line: ");
+double secondCoordinateYA = int.Parse(Console.ReadLine());
+Console.Write("Enter number of Z coordinate of the A line: ");
+double thirdCoordinateZA = int.Parse(Console.ReadLine());
+
+Console.Write("Enter number of X coordinate of the B line: ");
+double firstCoordinateXB = int.Parse(Console.ReadLine());
+Console.Write("Enter number of Y coordinate of the B line: ");
+double secondCoordinateYB = int.Parse(Console.ReadLine());
+Console.Write("Enter number of Z coordinate of the B line: ");
+double thirdCoordinateZB = int.Parse(Console.ReadLine());
+
+double distance = Math.Sqrt(Math.Pow(firstCoordinateXB - firstCoordinateXA, 2) + Math.Pow(secondCoordinateYB - secondCoordinateYA, 2) + Math.Pow(thirdCoordinateZB - thirdCoordinateZA, 2));
+
+Console.WriteLine($"Distance between two coordinates is {Math.Round(distance, 2)}");
---
+++ home_task_3/Task023/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+Console.Write("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+for (int count = 1; count <= number; count++)
+{
+	if (count < number)
+	{
+		Console.Write(Math.Pow(count, 3) + ", ");
+	}
+	else
+	{
+		Console.WriteLine(Math.Pow(count, 3) + ".");
+	}
+}
