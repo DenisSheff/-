238f745134ca7f22f2ceb32b1a5bf83d0154f879
Denis Shevchenko <denis_sheff@icloud.com>
2022-10-14 15:16:47 +0900

    Hometask

--- home_task_4/Task029/Program.cs
+++ home_task_4/Task029/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+int[] array = new int[8];
+
+void FilledArray(int[] collection)
+{
+	for (int i = 1; i < collection.Length; i++)
+	{
+		collection[i] = new Random().Next(0, 100);
+		Console.Write($"{array[i]} ");
+	}
+	Console.WriteLine();
+}
+
+
+FilledArray(array);
