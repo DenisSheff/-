7295863053f242dc31f8b4433005fe18f0930ed4
Denis Shevchenko <denis_sheff@icloud.com>
2022-10-14 14:59:47 +0900

    Home Task

---
+++ home_task_4/Task025/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+Console.Write("Enter first number: ");
+int firstNumber = int.Parse(Console.ReadLine());
+Console.Write("Enter second number: ");
+int secondNumber = int.Parse(Console.ReadLine());
+
+void PowerOfNumber()
+{
+	int powerIs = 1;
+	for (int i = 0; i < secondNumber; i++)
+	{
+		powerIs *= firstNumber;
+	}
+	Console.WriteLine("{0} ^ {1} = {2}", firstNumber, secondNumber, powerIs);
+}
+
+PowerOfNumber();
--- lection/Task_2/Program.cs
+++ lection/Task_2/Program.cs
@@ -32,6 +32,21 @@ void SelectionSort(int[] array)
 	}
 }
 
+void SelectionSortConvert(int[] array)
+{
+	for (int i = 0; i < array.Length - 1; i++)
+	{
+		int minPosition = i;
+		for (int j = i + 1; j < array.Length; j++)
+		{
+			if (array[j] > array[minPosition]) minPosition = j;
+		}
+		int temporary = array[i];
+		array[i] = array[minPosition];
+		array[minPosition] = temporary;
+	}
+}
+
 
 
 //Перевернуть массив, чтобы начиналось 9 8 7 ...
@@ -39,6 +54,7 @@ PrintArray(arr);
 SelectionSort(arr);
 
 PrintArray(arr);
+SelectionSortConvert(arr);
 
 
 PrintArray(arr);
