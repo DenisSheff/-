f21272bc518e123d1c3d3c73a566c66e76928b96
Denis Shevchenko <denis_sheff@icloud.com>
2022-10-14 15:00:31 +0900

    Folders

---
+++ class_task/Task026/Program.cs
@@ -0,0 +1,24 @@
+﻿/*
+Напишите программу, которая принимает
+на вход число и выдаёт количество цифр в числе.
+456 -> 3
+78 -> 2
+89126 -> 5 
+*/
+
+Console.Clear();
+Console.WriteLine("Enter a number: ");
+int number = int.Parse(Console.ReadLine());
+
+int GetQuantityOfDigits()
+{
+	int digits = 0;
+	while (number > 0)
+	{
+		number /= 10;
+		digits++;
+	}
+	return digits;
+}
+
+Console.WriteLine($"Quantity of your digits is {GetQuantityOfDigits()}.");
